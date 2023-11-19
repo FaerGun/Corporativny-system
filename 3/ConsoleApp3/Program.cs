18907bd964fb9d0454abf156d217d0604d7ac5a3
FaerGun <57860661+FaerGun@users.noreply.github.com>
2023-11-19 12:46:08 +0300

    Add files via upload

---
+++ 3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Инициализируем число L и последовательность строк
+        int L = 5;
+        string[] sequence = { "12345", "67890", "abcde", "fghij", "klmno", "pqrst", "uvwxy", "z1234" };
+
+        // Находим последнюю строку, начинающуюся с цифры и имеющую длину L
+        string lastStringStartingWithDigit = sequence.LastOrDefault(s => char.IsDigit(s[0]) && s.Length == L);
+
+        // Выводим результат
+        if (lastStringStartingWithDigit != null)
+        {
+            Console.WriteLine("Последняя строка, начинающаяся с цифры и имеющая длину " + L + ": " + lastStringStartingWithDigit);
+        }
+        else
+        {
+            Console.WriteLine("Not found");
+        }
+    }
+}
